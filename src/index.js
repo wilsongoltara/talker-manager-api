c923b44578b29319ae904cf677f93978fb945474
Wilson S. Goltara <wilson.goltara15@gmail.com>
2022-09-06 17:29:08 -0300

    initial commit in project talker_manager

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+  response.status(HTTP_OK_STATUS).send({ message: 'hello, world!' });
 });
 
 app.listen(PORT, () => {
