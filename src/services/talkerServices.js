31a7ad08fd8b09d3284e19a7ac9337310b29bd9d
Wilson S. Goltara <wilson.goltara15@gmail.com>
2022-09-08 18:22:45 -0300

    feat: create service get talker by id

--- src/services/talkerServices.js
+++ src/services/talkerServices.js
@@ -1,16 +1,30 @@
 const fs = require('fs/promises');
+const notFoundMessage = require('../util/customMessage');
 
-const filePath = 'src/talker.json';
+const filepath = 'src/talker.json';
 
 const getTalkers = async () => {
   try {
-    const talkers = await fs.readFile(filePath, 'utf8');
+    const talkers = await fs.readFile(filepath, 'utf8');
     return JSON.parse(talkers);
-  } catch (error) {
-    console.error(`error read file: ${error}`);    
+  } catch (err) {
+    return err;    
+  }
+};
+
+const getTalkerById = async (id) => {
+  try {
+    const talkers = await getTalkers();
+    const talkerFound = talkers.find((talker) => talker.id === Number(id));
+    return talkerFound
+      ? { result: talkerFound, statusCode: 200 }
+      : { result: notFoundMessage, statusCode: 404 };
+  } catch (err) {
+    return err;
   }
 };
 
 module.exports = {
   getTalkers,
+  getTalkerById,
 };
