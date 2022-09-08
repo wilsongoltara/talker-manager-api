731530fe1b98bc3cae5fb739579ec547d5ac3672
Wilson S. Goltara <wilson.goltara15@gmail.com>
2022-09-08 17:08:42 -0300

    feat: Req. 1 - add route in app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const talkersRouter = require('./routes/talkerRouter');
+
+const HTTP_OK_STATUS = 200;
 
 const app = express();
 app.use(bodyParser.json());
 
-const HTTP_OK_STATUS = 200;
-const PORT = '3000';
-
-// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send({ message: 'hello, world!' });
 });
 
+app.use('/talker', talkersRouter);
+
+// start server
+const PORT = '3000';
 app.listen(PORT, () => {
-  console.log('Online');
+  console.log(`listening on port ${PORT}`);
 });
